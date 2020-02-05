4e7c305e769c80f7dbdae7e379676d74c2e8fdae
HongyiLU <adamlu389535891@gamil.com>
2020-02-05 23:50:49 +0100

    Integrate with angular test form

--- src/main/java/com/projettic/controller/SqlTestController.java
+++ src/main/java/com/projettic/controller/SqlTestController.java
@@ -1,7 +1,9 @@
 package com.projettic.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.projettic.entity.Hero;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,13 +16,21 @@ public class SqlTestController {
     public String helloTest(){
         System.out.println("Hello Spring MVC");
         return "success";
-
     }
 
     @RequestMapping(path = "/sqltest")
-    public String testSql() throws IOException {
+    public String sqltest(){
         return "sql_query";
     }
 
+    @CrossOrigin(value="http://localhost:4200")
+    @RequestMapping(path = "/testsql", method = RequestMethod.POST)
+    public String testsql(@RequestBody String param, HttpServletRequest request) {
+        System.out.println(request.getMethod());
+        System.out.println(request.getHeader("Content-Type"));
+        Hero hero = JSON.parseObject(param, Hero.class);
+        System.out.println(hero.toString());
+        return "success";
+    }
 
 }
