e75f6d37809e1f26e996372abc20aa85c27af62f
HongyiLU <adamlu389535891@gamil.com>
2020-02-20 22:00:56 +0100

    add log function, add user register function, change maven setting

--- src/main/java/com/projettic/dao/EmpDao.java
+++ src/main/java/com/projettic/dao/EmpDao.java
@@ -4,10 +4,7 @@ import com.projettic.entity.Account;
 import com.projettic.entity.SqlQuery;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.jdbc.BadSqlGrammarException;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.List;
 
--- src/main/java/com/projettic/service/EmpService.java
+++ src/main/java/com/projettic/service/EmpService.java
@@ -2,8 +2,6 @@ package com.projettic.service;
 
 import com.projettic.entity.SqlQuery;
 
-import java.sql.SQLException;
-
 public interface EmpService {
     String getHisRes(SqlQuery sqlQuery);
     String getCorrection();
